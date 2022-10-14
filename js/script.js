9b79c0a51c6659bea8a44e76d0786aa514834a08
Wonski312 <smarciszedu@gmail.com>
2022-10-14 04:49:08 +0100

    project done

--- js/script.js
+++ js/script.js
@@ -1,20 +1,45 @@
-const submitBtn = document.querySelector('.button');
-const ratingDiv = document.querySelector('.rating-card__rate');
-const rateBtns = document.querySelectorAll('.rating-card__rate-button')
-console.log(rateBtns);
+const submitBtn = document.querySelector(".button");
+const ratingDiv = document.querySelector(".rating-card__rate");
+const rateBtns = document.querySelectorAll(".rating-card__rate-button");
+const thankYou = document.querySelector(".thank-you");
+const ratingCard = document.querySelector(".rating-card");
+const userSelection = document.querySelector(".userSelection");
 
-rateBtns.forEach((btn)=>{
-    btn.addEventListener('click', function(event){
-        console.log(event.target.value);
-        btn.classList.add('selected');
-})})
+let userSelect;
 
-const submitHander = (event) =>{
-    event.preventDefault();
-console.log('submited');
-}
+rateBtns.forEach((btn) => {
+	
+    btn.addEventListener("click", function (event) {
+		userSelect = event.target.value;
+		btn.classList.add("selected");
+	});
 
-submitBtn.addEventListener('click', submitHander);
-// rateBtns.addEventListener('click', function(event){
-// console.log(event.target.value);
-// })
+    btn.addEventListener('mouseover',  (event)=>{
+        const button = event.target;
+    const sibling = button.previousElementSibling;
+    if(button.value == '1'){
+        return 
+    }
+    sibling.classList.toggle('mouseOver');
+})
+btn.addEventListener('mouseout',  (event)=>{
+    const button = event.target;
+    const sibling = button.previousElementSibling;
+    if(button.value == '1'){
+        return 
+    }
+    sibling.classList.toggle('mouseOver');
+    })
+});
+
+const submitHander = (event) => {
+	event.preventDefault();
+	console.log("submited");
+	ratingCard.style.display = "none";
+	thankYou.style.display = "flex";
+
+	userSelection.textContent = userSelect;
+};
+
+submitBtn.addEventListener("click", submitHander);
+
